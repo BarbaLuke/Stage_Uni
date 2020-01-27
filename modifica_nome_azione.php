4a7360b62b9337669c998fa0f4dcd9f35b7cf82a
BarbaLuke <57746609+BarbaLuke@users.noreply.github.com>
2020-01-27 11:41:31 +0100

    bug correction

--- modifica_nome_azione.php
+++ modifica_nome_azione.php
@@ -4,8 +4,8 @@ $nome_nuovo = $_POST['nome_nuovo'];
 $nome_vecchio = $_POST['nome_vecchio'];
 
 //con questo apro il JSON delle azioni e leggo tutti i valori inserendoli dentro una variabile   
-$myfile = fopen("azioni_globali.json", "r") or die("Unable to open file!");
-$txt = fread($myfile,filesize("azioni_globali.json"));
+$myfile = fopen("azioni_global.json", "r") or die("Unable to open file!");
+$txt = fread($myfile,filesize("azioni_global.json"));
 fclose($myfile);
 
 // decodifico la variabile dove ho inserito tutti i valori per farla diventare un'array
@@ -21,7 +21,7 @@ foreach($azioni_array as $azion => $entry){
 }
 
 // infine apro ancora il file ma stavolta in scrittura e salvo l'array con il nuovo elemento modificato
-$file = fopen("azioni_globali.json","w");
+$file = fopen("azioni_global.json","w");
 fwrite($file, json_encode($azioni_array));
 fclose($file);
 exit;
